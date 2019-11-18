c3d089008af499d3daff1ff1e61008762b76cfbd
Julen Corral <julencv@gmail.com>
2019-11-18 22:43:23 +0100

    Lab finished without bonus

---
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -0,0 +1,56 @@
+/* Challenge 1 - Who Have Published What At Where? */
+
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME",
+authors.au_fname AS "FIRST NAME", titles.title AS "TITLE", publishers.pub_name AS "PUBLISHER"
+FROM titles
+JOIN publishers
+ON publishers.pub_id = titles.pub_id
+JOIN titleauthor
+ON titleauthor.title_id = titles.title_id
+JOIN authors
+ON titleauthor.au_id = authors.au_id;
+
+
+/*Challenge 2 - Who Have Published How Many At Where? */
+
+SELECT authors.au_id AS "AUTHOR ID", authors.au_lname AS "LAST NAME",
+authors.au_fname AS "FIRST NAME", publishers.pub_name AS "PUBLISHER",
+COUNT(titles.title) AS "TITLE COUNT"
+FROM titles
+JOIN publishers
+ON publishers.pub_id = titles.pub_id
+JOIN titleauthor
+ON titleauthor.title_id = titles.title_id
+JOIN authors
+ON titleauthor.au_id = authors.au_id
+GROUP BY authors.au_fname, publishers.pub_name
+ORDER BY COUNT(titles.title) DESC, authors.au_fname, authors.au_lname
+
+/*Challenge 3 - Best Selling Authors */
+
+SELECT authors.au_id AS "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+COUNT(titles.title) AS "TOTAL"
+FROM authors
+JOIN titleauthor
+ON titleauthor.au_id = authors.au_id
+JOIN titles
+ON titles.title_id = titleauthor.title_id
+GROUP BY authors.au_fname, authors.au_lname
+ORDER BY COUNT(titles.title) DESC
+LIMIT 3;
+
+/*Challenge 4 - Best Selling Authors Ranking */
+
+SELECT authors.au_id AS "AUTHOR ID", 
+authors.au_lname AS "LAST NAME", authors.au_fname AS "FIRST NAME",
+COUNT(titles.title) AS "TOTAL"
+FROM authors
+LEFT JOIN titleauthor
+ON titleauthor.au_id = authors.au_id
+LEFT JOIN titles
+ON titles.title_id = titleauthor.title_id
+GROUP BY authors.au_fname, authors.au_lname
+ORDER BY COUNT(titles.title) DESC;
+
+
